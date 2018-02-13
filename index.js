b85c00a69597812536808f4c04b8890e034fbfc0
Alyssa LaPerna <aalaperna@gmail.com>
2018-02-13 21:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function dwarfRollCall(dwarves) {
   var dwarfRollCall = []
   for (let i = 0; i < dwarfRollCall.length; i++) {
     dwarfRollCall.push(` ${i+1}. ${dwarfRollCall.length[i]}`)
-    return ${dwarfRollCall}
+    return (`${dwarfRollCall}`)
   }
 }
 
